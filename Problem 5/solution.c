88f7a840e292735ddac5b601a26d1768ef0074e1
Đỗ Thế Sang <79999787+Sang0920@users.noreply.github.com>
2023-07-19 01:29:51 +0700

    Create solution.c

---
+++ Problem 5/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int gcd(int a, int b){
+    if(a == 0) return b;
+    return gcd(b%a, a);
+}
+
+long smallest_multiple(int n){
+    long out = 1;
+    for (int i = 1; i<=n; i++){
+        out = (out*i)/gcd(out, i);
+    }
+    return out;
+}
+
+int main(){
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        int n; 
+        scanf("%d",&n);
+        printf("%ld\n", smallest_multiple(n));
+    }
+    return 0;
+}   
